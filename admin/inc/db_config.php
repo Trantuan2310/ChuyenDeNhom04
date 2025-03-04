45da38608f1a453918ffc6b235e016c567ddb5bd
tandung0103 <ntandung112003@gmail.com>
2025-03-04 11:05:38 +0700

    change

--- admin/inc/db_config.php
+++ admin/inc/db_config.php
@@ -2,7 +2,7 @@
   $hname = 'localhost';
   $uname = 'root';
   $pass = '';
-  $db = 'homestay';
+  $db = 'test';
 
   $con = mysqli_connect($hname,$uname,$pass,$db);
 
--- admin/inc/essentials.php
+++ admin/inc/essentials.php
@@ -1,7 +1,7 @@
 <?php 
 
 
-  define('SITE_URL','http://localhost:/homestay/');
+  define('SITE_URL','http://localhost:/test/');
   define('ABOUT_IMG_PATH',SITE_URL.'images/about/');
   define('CAROUSEL_IMG_PATH',SITE_URL.'images/carousel/');
   define('FACILITIES_IMG_PATH',SITE_URL.'images/facilities/');
